ceabe5e42b405f74e4e115a63b12799b780b3f0f
ynarenthiran <narenthiran4019@gmail.com>
2020-06-11 12:02:34 +0530

    footer changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Task';
+  lists: any = [
+    {
+      text: 'Set your professional profile website',
+      hint: 'Set yourself up for success'
+    },
+    {
+      text: 'Connect with B2B Search',
+      hint: 'Keep in touch and find opportunities'
+    },
+    {
+      text: 'Find the News & Insights',
+      hint: 'Get inspired in your ...'
+    }
+  ];
+  icons: any = [
+    {
+      label: 'Facebook',
+      icon: 'assets/images/facebook.svg'
+    },
+    {
+      label: 'Twitter',
+      icon: 'assets/images/twitter.svg'
+    },
+    {
+      label: 'Instagram',
+      icon: 'assets/images/instagram.svg'
+    }
+  ];
 }
